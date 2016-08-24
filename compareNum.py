c958bf1d885c4aba102c78b5ce16b45b00780431
cypeng <pcy4656822@163.com>
2016-08-24 14:18:47 +0800

    first code
    first code

---
+++ compareNum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import random
+
+def compareNum(num1,num2):
+	if (num1 > num2):
+		return 1
+	elif (num1 == num2):
+		return 0
+	else:
+		return -1
+
+num1 = random.randrange(1,9)
+num2 = random.randrange(1,9)
+
+print num1
+print num2
+print compareNum(num1,num2)
